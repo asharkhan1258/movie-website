354673377f0dae738c4c686d7bc07fb06f5b36b6
rehman-786 <abdulrehman@leilanitech.com>
2024-12-28 03:21:39 +0500

    Completed Signup and Login page design

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,42 +1,55 @@
-import { useState } from 'react';
-import { Form, Button } from 'react-bootstrap';
-import { Link, useNavigate } from 'react-router-dom';
-import axios from 'axios';
+import { useState } from "react";
+import { Form, Button } from "react-bootstrap";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Signin = ({ setIsAuthenticated }) => {
   const [formData, setFormData] = useState({
-    email: '',
-    password: ''
+    email: "",
+    password: "",
   });
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
   const navigate = useNavigate();
 
   const handleChange = (e) => {
     setFormData({
       ...formData,
-      [e.target.name]: e.target.value
+      [e.target.name]: e.target.value,
     });
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post(`${import.meta.env.VITE_API_URL}/auth/signin`, formData);
-      localStorage.setItem('token', response.data.token);
+      const response = await axios.post(
+        `${import.meta.env.VITE_API_URL}/auth/signin`,
+        formData
+      );
+      localStorage.setItem("token", response.data.token);
       setIsAuthenticated(true);
-      navigate('/movies');
+      navigate("/movies");
     } catch (err) {
-      setError(err.response?.data?.message || 'An error occurred');
+      setError(err.response?.data?.message || "An error occurred");
     }
   };
 
   return (
     <div className="auth-container">
-      <h2 className="mb-4">Sign in</h2>
+      <h2 className="mb-5 auth-title">Sign in</h2>
       {error && <p className="text-danger mb-3">{error}</p>}
-      <Form onSubmit={handleSubmit} style={{ width: '100%', maxWidth: '400px' }}>
+      <Form
+        onSubmit={handleSubmit}
+        style={{
+          width: "100%",
+          maxWidth: "400px",
+          display: "flex",
+          flexDirection: "column",
+          gap: "9px",
+        }}
+      >
         <Form.Group className="mb-3">
           <Form.Control
+            className="input"
             type="email"
             name="email"
             placeholder="Email"
@@ -47,6 +60,7 @@ const Signin = ({ setIsAuthenticated }) => {
         </Form.Group>
         <Form.Group className="mb-3">
           <Form.Control
+            className="input"
             type="password"
             name="password"
             placeholder="Password"
@@ -55,18 +69,23 @@ const Signin = ({ setIsAuthenticated }) => {
             required
           />
         </Form.Group>
-        <Form.Group className="mb-3">
-          <Form.Check 
-            type="checkbox" 
-            label="Remember me" 
-            className="text-white"
+        <Form.Group className="mb-3 input d-flex justify-content-center">
+          <Form.Check
+            type="checkbox"
+            label="Remember me"
+            className="text-white checkbox"
           />
         </Form.Group>
-        <Button variant="primary" type="submit" className="w-100 mb-3">
-          Login
-        </Button>
-        <p className="text-center text-white">
-          To register <Link to="/signup" className="text-primary">Sign up</Link>
+        <div className="login-button">
+          <Button variant="primary" type="submit" className="mb-3 w-100">
+            Login
+          </Button>
+        </div>
+        <p className="text-center text-white bottom-desc">
+          To register{" "}
+          <Link to="/signup" className="text-primary bottom-link">
+            Sign up
+          </Link>
         </p>
       </Form>
     </div>
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,42 +1,55 @@
-import { useState } from 'react';
-import { Form, Button } from 'react-bootstrap';
-import { Link, useNavigate } from 'react-router-dom';
-import axios from 'axios';
+import { useState } from "react";
+import { Form, Button } from "react-bootstrap";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Signup = ({ setIsAuthenticated }) => {
   const [formData, setFormData] = useState({
-    email: '',
-    password: ''
+    email: "",
+    password: "",
   });
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
   const navigate = useNavigate();
 
   const handleChange = (e) => {
     setFormData({
       ...formData,
-      [e.target.name]: e.target.value
+      [e.target.name]: e.target.value,
     });
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post(`${import.meta.env.VITE_API_URL}/auth/signup`, formData);
-      localStorage.setItem('token', response.data.token);
+      const response = await axios.post(
+        `${import.meta.env.VITE_API_URL}/auth/signup`,
+        formData
+      );
+      localStorage.setItem("token", response.data.token);
       setIsAuthenticated(true);
-      navigate('/movies');
+      navigate("/movies");
     } catch (err) {
-      setError(err.response?.data?.message || 'An error occurred');
+      setError(err.response?.data?.message || "An error occurred");
     }
   };
 
   return (
     <div className="auth-container">
-      <h2 className="mb-4">Sign up</h2>
+      <h2 className="mb-5 auth-title">Sign up</h2>
       {error && <p className="text-danger mb-3">{error}</p>}
-      <Form onSubmit={handleSubmit} style={{ width: '100%', maxWidth: '400px' }}>
+      <Form
+        onSubmit={handleSubmit}
+        style={{
+          width: "100%",
+          maxWidth: "400px",
+          display: "flex",
+          flexDirection: "column",
+          gap: "9px",
+        }}
+      >
         <Form.Group className="mb-3">
           <Form.Control
+            className="input"
             type="email"
             name="email"
             placeholder="Email"
@@ -47,6 +60,7 @@ const Signup = ({ setIsAuthenticated }) => {
         </Form.Group>
         <Form.Group className="mb-3">
           <Form.Control
+            className="input"
             type="password"
             name="password"
             placeholder="Password"
@@ -55,11 +69,16 @@ const Signup = ({ setIsAuthenticated }) => {
             required
           />
         </Form.Group>
-        <Button variant="primary" type="submit" className="w-100 mb-3">
-          Sign up
-        </Button>
-        <p className="text-center text-white">
-          Already have an account? <Link to="/signin" className="text-primary">Sign in</Link>
+        <div className="signup-button">
+          <Button variant="primary" type="submit" className="mb-3 w-100">
+            Sign up
+          </Button>
+        </div>
+        <p className="text-center text-white bottom-desc">
+          Already have an account?{" "}
+          <Link to="/signin" className="text-primary bottom-link">
+            Sign in
+          </Link>
         </p>
       </Form>
     </div>
